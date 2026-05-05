b4e4ddc6ee597d405f28448c9c5c9544b22d5c81
rabbitboo <destimelani33@gmail.com>
2026-05-05 15:17:22 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Biarkan kosong untuk PWA basic, atau tambahkan logika caching nanti
-});
